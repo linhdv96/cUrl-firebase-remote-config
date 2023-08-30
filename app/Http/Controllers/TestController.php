54292acdf68e3c2324d4370d03682353dbf3f188
linhdv96 <dinhvanlinh96@gmail.com>
2023-08-30 14:55:11 +0700

    v2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\Cache;
+
 class TestController extends Controller
 {
     public function get_token()
@@ -27,7 +29,9 @@ class TestController extends Controller
             $client->authenticate($_GET['code']);
             $token = $client->getAccessToken();
             $access_token = $token['access_token'];
-            dd($access_token, $client);
+            Cache::forever('google_access_token', $access_token);
+            echo 'Đã cập nhật access token';
+            die();
         }
 
         $auth_url = $client->createAuthUrl();
@@ -38,8 +42,7 @@ class TestController extends Controller
     public function firebase_remote_config()
     {
         // ----- get list remote config
-        $access_token = 'ya29.a0AWY7CkkspEf4Ep9sWYUO4kSU_gJVXQLlxN-FiDGE98cUxJP9QuPneKTE-oEwBcNVwDD4yc6K1y7svgr0SOY-_LzM9G6FXDtosiQCvkWpcY67dltARNPCJOUumezMnhdR0OWWRvcl-t7YweIb4pIcIS4m5yKMaCgYKAV0SARISFQG1tDrp_lmgWRuyTOyKWx5V8MdFRg0163 ◀ya29.a0AWY7CkkspEf4Ep9sWYUO4kSU_gJVXQLlxN-FiDGE98cUxJP9QuPneKTE-oEwBcNVwDD4yc6K1y7svgr0SOY-_LzM9G6FXDtosiQCvkWpcY67dltARNPCJOUumezMnhdR0OWWRvcl-t7YweIb4pIcIS4m5';
-        $project_id = 'myproject3-3d911';
+        $access_token = 'ya29.a0AWY7CkmG2RvITxkJgPqgtXAurkNGqAZW6hK6BHef4UuIc5z1TjZtMlKmXP1hLfwBay8u0sk054ZKUXPrzBqLDT-uu6vFu_lp5LEWNyohprMKUtJdvXH1Nncy-Gl9u2Z_fkbkF38JK--P1sQqXwSss0HALRWSaCgYKAUASARISFQG1tDrps0rgDG8KoU0lAxcZGUCT-g0163 ◀ya29.a0AWY7CkmG2RvITxkJgPqgtXAurkNGqAZW6hK6BHef4UuIc5z1TjZtMlKmXP1hLfwBay8u0sk054ZKUXPrzBqLDT-uu6vFu_lp5LEWNyohprMKUtJdvXH1Nncy-Gl9u2Z_fkbkF38JK--P1sQqXwSss0HAL';
         $project_id = 'myproject2-27cf6';
 
         $curl = curl_init();
@@ -74,4 +77,24 @@ class TestController extends Controller
 //        curl_close($curl);
 //        dd($response2);
     }
+
+//    public function getToken2(){
+//        $client_id = "";
+//        $client_secret = "";
+//        $refresh_token = "";
+//
+//        $curl_token = curl_init();
+//        $URL = "https://oauth2.googleapis.com/token";
+//        $header_token[] = "Content-Type: application/x-www-form-urlencoded";
+//        $body = "client_id=". $client_id ."&client_secret=". $client_secret ."&refresh_token=". $refresh_token ."&grant_type=refresh_token";
+//        curl_setopt($curl_token, CURLOPT_HTTPHEADER, $header_token);
+//        curl_setopt($curl_token, CURLOPT_POSTFIELDS, $body);
+//        curl_setopt($curl_token, CURLOPT_URL, $URL);
+//        curl_setopt($curl_token, CURLOPT_CUSTOMREQUEST, "POST");
+//        curl_setopt($curl_token, CURLOPT_RETURNTRANSFER, true);
+//        $response_token = curl_exec($curl_token);
+//        curl_close($curl_token);
+//        $response_token = json_decode($response_token, true);
+//        $access_token = $response_token['access_token'];
+//    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\TestController::class, 'get_token']);
 Route::get('/firebase_remote_config', [\App\Http\Controllers\TestController::class, 'firebase_remote_config']);
+
+
+Route::get('/listGame', [\App\Http\Controllers\GameController::class, 'listGame']);
+Route::post('/change_cf_game_promo', [\App\Http\Controllers\GameController::class, 'change_cf_game_promo'])->name('game.change_cf_game_promo');
